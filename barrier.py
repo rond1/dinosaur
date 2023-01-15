c2cad115c4ad27eb93e8eee9a6214b0f337c7a96
rond1 <kenssske@yandex.ru>
2023-01-15 16:58:33 +0300

    a class for birds has been added. the cactus class and the bird class were combined

---
+++ barrier.py
@@ -0,0 +1,49 @@
+import pygame
+import random
+
+
+class BarrierSprite(pygame.sprite.Sprite):
+    def __init__(self, all_sprites, sheet, speed, x, y):
+        super().__init__(all_sprites)
+        self.random_size = [[[230, 3, 15, 33], [246, 3, 32, 33], [280, 3, 49, 33]],
+                            [[333, 3, 23, 46], [356, 3, 50, 46], [408, 3, 73, 46]], [[182, 4, 41, 25]],
+                            [[230, 3, 15, 33], [246, 3, 32, 33], [280, 3, 49, 33]],
+                            [[333, 3, 23, 46], [356, 3, 50, 46], [408, 3, 73, 46]],
+                            [[230, 3, 15, 33], [246, 3, 32, 33], [280, 3, 49, 33]],
+                            [[333, 3, 23, 46], [356, 3, 50, 46], [408, 3, 73, 46]]]
+        self.part_of_sheet = [333, 3, 23, 46]
+        self.image = sheet.subsurface(pygame.Rect([333, 3, 23, 46]))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.sheet = sheet
+        self.speed = speed
+        self.count = 0
+        self.position = -1
+
+    def update(self):
+        self.rect.x -= self.speed
+        if self.rect.x + self.part_of_sheet[2] < 0:
+            self.part_of_sheet = random.choice(random.choice(self.random_size))
+            self.image = self.sheet.subsurface(pygame.Rect(self.part_of_sheet))
+            self.rect = self.image.get_rect()
+            self.rect.x = 600
+            if self.part_of_sheet in self.random_size[1]:
+                self.rect.y = 97
+                self.position = -1
+            elif self.part_of_sheet in self.random_size[0]:
+                self.rect.y = 108
+                self.position = -1
+            else:
+                self.rect.y = random.choice([10, 70, 90])
+                self.count = 0
+                self.position = 1
+        elif self.position >= 0:
+            if self.count % 7 == 0:
+                if self.position:
+                    self.image = self.sheet.subsurface(pygame.Rect(136, 10, 41, 30))
+                    self.position = 0
+                else:
+                    self.image = self.sheet.subsurface(pygame.Rect(182, 4, 41, 25))
+                    self.position = 1
+            self.count += 1
--- bird.py
+++ bird.py
@@ -6,7 +6,7 @@ import random
 
 from road import RoadSprite
 from cloud import CloudSprite
-from barrier import BarrierSprite
+from cactus import CactusSprite
 
 
 size = width, height = 600, 150
@@ -35,6 +35,34 @@ def load_image(name, colorkey=None):
     return image
 
 
+class BirdSprite(pygame.sprite.Sprite):
+    def __init__(self, all_sprites, sheet, speed, x, y):
+        super().__init__(all_sprites)
+        self.width = 41
+        self.sheet = sheet
+        self.image = sheet.subsurface(pygame.Rect(182, 4, self.width, 25))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = speed
+        self.count = 0
+        self.position = 1
+
+    def update(self):
+        self.rect.x -= self.speed
+        if self.count % 7 == 0:
+            if self.position:
+                self.image = self.sheet.subsurface(pygame.Rect(136, 10, self.width, 30))
+                self.position = 0
+            else:
+                self.image = self.sheet.subsurface(pygame.Rect(182, 4, self.width, 25))
+                self.position = 1
+        if self.rect.x + self.width < 0:
+            self.rect.x = 600
+            self.rect.y = random.choice([10, 70, 90])
+        self.count += 1
+
+
 running = True
 screen.fill((255, 255, 255))
 sheet = load_image("all.png")
@@ -42,8 +70,9 @@ all_sprites = pygame.sprite.Group()
 cloud1 = CloudSprite(all_sprites, sheet, cloud_speed, 0, random.randint(30, 70))
 cloud2 = CloudSprite(all_sprites, sheet, cloud_speed, 200, random.randint(30, 70))
 cloud3 = CloudSprite(all_sprites, sheet, cloud_speed, 400, random.randint(30, 70))
-barrier1 = BarrierSprite(all_sprites, sheet, road_speed, 600, 97)
-barrier2 = BarrierSprite(all_sprites, sheet, road_speed, 900, 97)
+cactus1 = CactusSprite(all_sprites, sheet, road_speed, 600, 97)
+bird = BirdSprite(all_sprites, sheet, road_speed, 600, random.choice([10, 70, 90]))
+cactus2 = CactusSprite(all_sprites, sheet, road_speed, 900, 97)
 road_start = RoadSprite(all_sprites, sheet, road_speed, 0, 129)
 road_end = RoadSprite(all_sprites, sheet, road_speed, road_start.width, 129)
 clock = pygame.time.Clock()
